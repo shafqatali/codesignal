d1d7381459d60b67609baa4049a1e06abc25bca1
Shafqat Ali <shafqatali28@gmail.com>
2020-09-12 17:54:27 +0500

    Update first_digit.js

--- firstDigit/first_digit.js
+++ firstDigit/first_digit.js
@@ -1,4 +1,10 @@
 function firstDigit($inputString) {
-    preg_match('/\d/', $inputString, $match);
-    return $match[0];
+        var number = "";
+    for(var c=0; c< inputString.length; c++){
+        var char = inputString[c];
+        if(isNaN(char) == false && char != " " && number == "") {
+            number = inputString[c];
+        }
+    }
+    return number;
 }
